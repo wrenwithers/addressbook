6b959e2393e79327f612208550a9d96b0949fca5
Wren Withers <wrenwithers@Wrens-MacBook-Air.local>
2016-01-26 13:30:00 -0800

    get address book contact basics running

--- specs/scripts.js
+++ specs/scripts.js
@@ -1,9 +1,31 @@
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+var inputtedFirstName = $("input#new-first-name").val();
+var inputtedLastName = $("input#new-last-name").val();
+var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
 function Contact(firstName,lastName) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.addresses = [];
+}
 
   Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
-}
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    $(".contact").last().click(function() {
+
+    $("#show-contact").show();
+    $("#show-contact h2").text(newContact.fullName());
+    $(".first-name").text(newContact.firstName);
+    $(".last-name").text(newContact.lastName);
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+});
+});
