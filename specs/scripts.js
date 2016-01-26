5b386d9b1569a5a6edf224c7db63a5e4169d678a
Wren Withers <wrenwithers@Wrens-MacBook-Air.local>
2016-01-26 11:37:35 -0800

    begin addressbook specs and script

---
+++ specs/scripts.js
@@ -0,0 +1,9 @@
+function Contact(firstName,lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.addresses = [];
+
+  Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+}
---
+++ specs/specs.js
@@ -0,0 +1,13 @@
+describe('Contact', function() {
+  it("creates a new contact with the given properties", function() {
+    var testContact = new Contact("Rita","Moreno");
+    expect(testContact.firstName).to.equal("Rita");
+    expect(testContact.lastName).to.equal("Moreno");
+    expect(testContact.addresses).to.eql([]);
+  });
+
+  it("adds the fullName method to all contacts", function() {
+    var testContact = new Contact("Sherlock","Holmes");
+    expect(testContact.fullName()).to.equal("Sherlock Holmes");
+});
+});
